e01080d3c0367b721bfa3cea09778c4059f45965
Azzoz2684 <rdoan2684@gmail.com>
2024-11-05 16:38:50 +0200

    Add files via upload

--- upload.php
+++ upload.php
@@ -2,6 +2,7 @@
 if ($_FILES["file"]) {
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["file"]["name"]);
+
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
         echo "تم تحميل الملف بنجاح.";
     } else {
